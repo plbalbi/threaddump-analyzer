8f501d3156671ee1c4323238b394fddc8ec039ad
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-04-03 20:48:27 -0300

    jps and jstack backend working

--- java_utils.js
+++ java_utils.js
@@ -4,26 +4,6 @@ var path = require('path');
 
 var app = express();
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.get('/processes', function (req, res) {
     // Do not forward stdout to console
     var runningProcessesLines = shell.exec("jps -l", { silent: true }).stdout
@@ -38,8 +18,10 @@ app.get('/processes', function (req, res) {
     res.send(runningProcesses);
 });
 
-app.get('/threaddump', (request, response) => {
-    console.log("Not yet implemented")
+app.get('/threaddump/:pid', (request, response) => {
+    const pid = request.params.pid
+    var threadDumpText = shell.exec("jstack -l " + pid, { silent: true }).stdout
+    response.send(threadDumpText)
 })
 
 app.get('/*', function(req, res) {
